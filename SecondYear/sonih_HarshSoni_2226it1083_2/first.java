25f7a1ddafbf0b2ed67224dbf7920ac50a4c55b9
Harsh soni <135133057+Harsh5soni@users.noreply.github.com>
2023-10-05 19:07:42 +0530

    added file

---
+++ SecondYear/sonih_HarshSoni_2226it1083_2/first.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class First {
+    public static void main()
+    int a,b,sum=0;
+    Scanner sc=new Scanner(System.in);
+    a=sc.nextInt();
+    b=sc.nextInt();
+    sum=a+b;
+    System.out.println(sum);
+}
